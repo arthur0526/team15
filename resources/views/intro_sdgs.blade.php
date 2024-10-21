71dfea29bdf62f7058cd7fe6152755302324de51
LAPTOP-2RF1NGV8\arthur <arthurt513kd@gmail.com>
2024-10-22 00:11:47 +0800

    sdgs目標

--- resources/views/intro_sdgs.blade.php
+++ resources/views/intro_sdgs.blade.php
@@ -69,111 +69,110 @@
     </a>
 
     <section id="goal1">
-        <h2><a href="https://globalgoals.tw/1-no-poverty">目標1：消除貧窮</a></h2>
+        <h2><a href="https://globalgoals.tw/1-no-poverty"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/121/m/mczh-tw400x400_small14157_145600714135.png"height="300" width="300"/><br>目標1：消除貧窮</a></h2>
         <div class="goal">
-            <p>到2030年，全球各地消除一切形式的貧窮，尤其是在最弱勢的群體中。</p>
+            <p>消除各地一切形式的貧窮</p>
         </div>
     </section>
  
     <section id="goal2">
-        <h2><a href="https://globalgoals.tw/2-zero-hunger">目標2：消除飢餓</a></h2>
+        <h2><a href="https://globalgoals.tw/2-zero-hunger"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/344/m/mczh-tw400x400_small14158_889074514124.png"height="300" width="300"/><br>目標2：終結飢餓</a></h2>
         <div class="goal">
-            <p>到2030年，消除飢餓，確保所有人擁有足夠的安全和營養食物。</p>
+            <p>消除飢餓，達成糧食安全，改善營養及促進永續農業</p>
         </div>
     </section>
     
     <section id="goal3">
-        <h2><a href="https://globalgoals.tw/3-good-health-and-well-being">目標3：良好健康與福祉</a></h2>
+        <h2><a href="https://globalgoals.tw/3-good-health-and-well-being"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/988/m/mczh-tw400x400_small14159_115028214112.png"height="300" width="300"/><br>目標3：健康與福祉</a></h2>
         <div class="goal">
             <p>確保健康的生活方式，促進各年齡段的福祉。</p>
         </div>
     </section>
     
     <section id="goal4">
-        <h2><a href="https://globalgoals.tw/4-quality-education">目標4：優質教育</a></h2>
+        <h2><a href="https://globalgoals.tw/4-quality-education"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/77/m/mczh-tw400x400_small14160_919410614100.png"height="300" width="300"/><br>目標4：優質教育</a></h2>
         <div class="goal">
             <p>確保包容和公平的優質教育，促進終身學習機會。</p>
         </div>
     </section>
     
     <section id="goal5">
-        <h2>目標5：性別平等</h2>
+        <h2><a href="https://globalgoals.tw/5-gender-equality"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/370/m/mczh-tw400x400_small14161_669501414088.png"height="300" width="300"/><br>目標5：性別平等</a></h2>
         <div class="goal">
             <p>實現性別平等，賦予所有女性和女孩權能。</p>
         </div>
     </section>
     
     <section id="goal6">
-        <h2>目標6：潔淨飲水與衛生設施</h2>
+        <h2><a href="https://globalgoals.tw/6-clean-water-and-sanitation "><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/40/m/mczh-tw400x400_small14162_280925214076.png"height="300" width="300"/><br>目標6：淨水與衛生</a></h2>
         <div class="goal">
             <p>確保全人類獲得水和衛生設施的可持續管理。</p>
         </div>
     </section>
     
     <section id="goal7">
-        <h2>目標7：經濟適用的清潔能源</h2>
+        <h2><a href="https://globalgoals.tw/7-affordable-and-clean-energy"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/959/m/mczh-tw400x400_small14163_464903314063.png"height="300" width="300"/><br>目標7：可負擔的永續能源</a></h2>
         <div class="goal">
             <p>確保所有人能獲得可負擔的、可靠的、可持續的現代能源。</p>
         </div>
     </section>
     
     <section id="goal8">
-        <h2>目標8：體面工作和經濟增長</h2>
+        <h2><a href="https://globalgoals.tw/8-decent-work-and-economic-growth"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/596/m/mczh-tw400x400_small14164_973687614051.png"height="300" width="300"/><br>目標8：就業與經濟成長</a></h2>
         <div class="goal">
             <p>促進持續、包容和可持續的經濟增長，實現充分和生產性就業。</p>
         </div>
     </section>
     
     <section id="goal9">
-        <h2>目標9：產業、創新和基礎設施</h2>
+        <h2><a href="https://globalgoals.tw/9-industry-innovation-and-infrastructure"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/419/m/mczh-tw400x400_small14165_308333814032.png"height="300" width="300"/><br>目標9：永續工業與基礎建設</a></h2>
         <div class="goal">
             <p>建設可持續的基礎設施，促進包容和可持續的工業化，鼓勵創新。</p>
         </div>
     </section>
     <section id="goal10">
-        <h2>目標10：減少不平等</h2>
+        <h2><a href="https://globalgoals.tw/10-reduced-inequalities"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/824/m/mczh-tw400x400_small14166_40658314018.png"height="300" width="300"/><br>目標10：消弭不平等</a></h2>
         <div class="goal">
             <p>在各國和國內減少不平等，確保每個人都能平等參與社會。</p>
         </div>
     </section>  
     <section id="goal11">
-        <h2>目標11：可持續城市和社區</h2>
+        <h2><a href="https://globalgoals.tw/11-sustainable-cities-and-communities"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/643/m/mczh-tw400x400_small14167_4909814004.png"height="300" width="300"/><br>目標11：永續城鄉</a></h2>
         <div class="goal">
             <p>使城市和人類住區包容、安全、具韌性和可持續。</p>
         </div>
     </section> 
     <section id="goal12">
-        <h2>目標12：負責任的消費和生產</h2>
+        <h2><a href="https://globalgoals.tw/12-responsible-consumption-and-production"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/190/m/mczh-tw400x400_small14168_70635413989.png"height="300" width="300"/><br>目標12：責任消費與生產</a></h2>
         <div class="goal">
             <p>確保可持續的消費和生產模式。</p>
         </div>
     </section>   
     <section id="goal13">
-        <h2>目標13：氣候行動</h2>
+        <h2><a href="https://globalgoals.tw/13-climate-action"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/936/m/mczh-tw400x400_small14169_444108613977.png"height="300" width="300"/><br>目標13：氣候行動</a></h2>
         <div class="goal">
             <p>加強應對氣候變化及其影響的行動。</p>
         </div>
     </section>
     <section id="goal14">
-        <h2>目標14：水下生物</h2>
-        <div class="goal">
+        <h2><a href="https://globalgoals.tw/14-life-below-water"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/418/m/mczh-tw400x400_small14170_910160913963.png"height="300" width="300"/><br>目標14：永續海洋與保育</a></h2>
             <p>保護和可持續使用海洋和海洋資源，以促進可持續發展。</p>
         </div>
     </section>   
     <section id="goal15">
-        <h2>目標15：陸地生物</h2>
+        <h2><a href="https://globalgoals.tw/15-life-on-land"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/117/m/mczh-tw400x400_small14171_845957613949.png"height="300" width="300"/><br>目標15：陸域生態</a></h2>
         <div class="goal">
             <p>保護、恢復和促進陸地生態系統的可持續利用，管理森林，防止沙漠化，停止土地劣化和生物多樣性的喪失。</p>
         </div>
     </section>   
     <section id="goal16">
-        <h2>目標16：和平與公正的社會</h2>
+        <h2><a href="https://globalgoals.tw/16-peace-justice-and-strong-institutions"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/75/m/mczh-tw400x400_small14172_275303013935.png"height="300" width="300"/><br>目標16：制度的正義與和平</a></h2>
         <div class="goal">
             <p>進和平和包容的社會，提供公正的法律制度</p>
         </div>
     </section>
     <section id="goal17">
-         <h2>目標17：強化永續發展執行方法及活化永續發展全球夥伴關係</h2>
+         <h2><a href="https://globalgoals.tw/17-partnerships-for-the-goals"><img src="https://lic.nkuht.edu.tw/var/file/7/1007/pictures/232/m/mczh-tw400x400_small14173_977191413921.png"height="300" width="300"/><br>目標17：永續發展夥伴關係</a></h2>
         <div class="goal">
             <p>加強執行手段，活化全球夥伴關係以促進可持續發展。</p>
         </div>
@@ -182,6 +181,6 @@
     <footer>
         <p>© 2024 SDGs 宣傳網站. All rights reserved.</p>
     </footer>
-
+    
 </body>
 </html>
