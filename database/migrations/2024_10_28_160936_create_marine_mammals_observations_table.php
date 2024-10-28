500c1157691f191308c333646f599c1683c9d2d3
LAPTOP-2RF1NGV8\arthur <arthurt513kd@gmail.com>
2024-10-29 01:59:46 +0800

    修改資料庫

--- database/migrations/2024_10_28_160936_create_marine_mammals_observations_table.php
+++ database/migrations/2024_10_28_160936_create_marine_mammals_observations_table.php
@@ -6,23 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateMarineMammalsObservationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('marine_mammals_observations', function (Blueprint $table) {
             $table->id(); // 自動遞增主鍵
             $table->integer('year')->comment('年份（民國年）'); // 年份（民國年）
-            $table->float('solar_power')->nullable()->comment('太陽光電發電量 (百萬度)'); // 太陽光電
-            $table->float('wind_power')->nullable()->comment('風力發電量 (百萬度)'); // 風力發電
-            $table->float('hydro_power')->nullable()->comment('慣常水力發電量 (百萬度)'); // 慣常水力
-            $table->float('biomass_power')->nullable()->comment('生質能發電量 (百萬度)'); // 生質能發電
+            $table->float('solar_power')->comment('太陽光電發電量 (百萬度)'); // 太陽光電
+            $table->float('wind_power')->comment('風力發電量 (百萬度)'); // 風力發電
+            $table->float('hydro_power')->comment('慣常水力發電量 (百萬度)'); // 慣常水力
+            $table->float('biomass_power')->comment('生質能發電量 (百萬度)'); // 生質能發電
             $table->float('geothermal_power')->nullable()->comment('地熱發電量 (百萬度)'); // 地熱發電
-            $table->float('waste_power')->nullable()->comment('廢棄物發電量 (百萬度)'); // 廢棄物發電
-            $table->float('total_power')->nullable()->comment('發電總合計 (百萬度)'); // 合計
+            $table->float('waste_power')->comment('廢棄物發電量 (百萬度)'); // 廢棄物發電
+            $table->float('total_power')->comment('發電總合計 (百萬度)'); // 合計
             $table->timestamps(); // 自動產生 created_at 和 updated_at 欄位
         });
     }
